1e06623d1b2264ede084e327c7bddc938af6695e
beccamason <beccamason28@gmail.com>
2022-02-15 11:14:36 +0000

    Added test for length of done items being greater than 5 for stretch goals - deferring further progress until other parts are complete

--- todo_app/test_viewmodel.py
+++ todo_app/test_viewmodel.py
@@ -5,8 +5,22 @@ from todo_app.data import trello_items
 
 @pytest.fixture
 def board_prep(): 
-    cards = [Card("1", "Not Started Test", "Not Started", "test"), Card("2", "In Progress Test", "In Progress", "test"), Card("3", "Done Test", "Done", "test")]
+    not_started_card = Card("1", "Not Started Test", "Not Started", "test")
+    in_progress_card = Card("2", "In Progress Test", "In Progress", "test")
+    done_card = Card("3", "Done Test", "Done", "test")
+    cards = [not_started_card, in_progress_card, done_card]
     return ViewModel(cards)
+
+@pytest.fixture
+def done_cards_prep():
+    done_card_1 = Card("3", "Done Test", "Done", "test")
+    done_card_2 = Card("4", "Done Test", "Done", "test")
+    done_card_3 = Card("5", "Done Test", "Done", "test")
+    done_card_4 = Card("6", "Done Test", "Done", "test")
+    done_card_5 = Card("7", "Done Test", "Done", "test")
+    done_card_6 = Card("8", "Done Test", "Done", "test")
+    done_cards = [done_card_1, done_card_2, done_card_3, done_card_4, done_card_5, done_card_6]
+    return ViewModel(done_cards)
 
 def test_pytest(): 
     assert(True)
@@ -25,3 +39,8 @@ def test_doing(board_prep):
     assert len(board_prep.doing_items) == 1
     assert board_prep.doing_items[0].title == "In Progress Test"
     assert board_prep.doing_items[0].status == "In Progress"
+
+def test_done_length(done_cards_prep):
+    if len(board_prep.done_items) > 5: 
+        assert len(board_prep.done_items_filtered) == 5
+    else: assert(True)
